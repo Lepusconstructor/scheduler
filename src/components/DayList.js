1f39bea84c220a64bb97e68fe115c1937fd61744
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-12-07 21:38:56 -0500

    building DayList component as well as story

---
+++ src/components/DayList.js
@@ -0,0 +1,20 @@
+import React from "react";
+//import classnames from "classnames";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const day = props.days.map((day, id) => {
+    return (
+    <ul>
+      <DayListItem 
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  />
+    </ul>
+  );
+  })
+  
+
+}
